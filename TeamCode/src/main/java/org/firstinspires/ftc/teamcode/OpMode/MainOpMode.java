b583af9ce67ddc4b87e6a9411136e2cc96fad47d
CaptainTNT <72236313+CaptainTNT@users.noreply.github.com>
2024-03-03 20:30:13 -0500

    Added MainOpMode and updetaed gradle

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/MainOpMode.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.OpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class MainOpMode extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
