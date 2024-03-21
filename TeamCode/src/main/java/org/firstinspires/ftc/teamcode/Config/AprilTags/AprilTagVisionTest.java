a0e26050178c20a5becadb89374fba17a6a9aba8
CaptainTNT <72236313+CaptainTNT@users.noreply.github.com>
2024-03-21 16:44:59 -0400

    Added April Tags

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Config/AprilTags/AprilTagVisionTest.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.Config.AprilTags;
+
+import android.util.Size;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+public class AprilTagVisionTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawTagOutline(true)
+                .build();
+
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(640, 480))
+                .build();
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            if (tagProcessor.getDetections().size() > 0){
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+
+                telemetry.addData("x", tag.ftcPose.x);
+                telemetry.addData("y", tag.ftcPose.y);
+                telemetry.addData("z", tag.ftcPose.z);
+                telemetry.addData("roll", tag.ftcPose.roll);
+                telemetry.addData("pitch", tag.ftcPose.pitch);
+                telemetry.addData("yaw", tag.ftcPose.yaw);
+                telemetry.addData("bearing", tag.ftcPose.bearing);
+                telemetry.addData("pitch", tag.ftcPose.pitch);
+            }
+
+            telemetry.update();
+        }
+    }
+}
